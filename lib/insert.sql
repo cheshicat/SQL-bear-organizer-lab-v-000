b72a66959d17f45ff90522a81dd5d5012abc7ff7
Allison Howard <alliemhow@gmail.com>
2018-12-18 12:17:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "Male" )
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "Male", "Brown" )
